350e6e8730e25eb1f8b987b61d5293461270fd90
Кирилл <kirill-kovalev>
2020-04-17 21:43:51 +0300

    Updated Favourite card view

--- flightApp/Views/favouriteCardView.swift
+++ flightApp/Views/favouriteCardView.swift
@@ -9,21 +9,30 @@
 import SwiftUI
 
 struct favouriteCardView: View {
+	@Binding var isSet:Bool
+	
     var body: some View {
-        ZStack(alignment: .topLeading){
-                   LinearGradient(gradient:
-                   Gradient(colors: [Color(UIColor(red: 184/256, green: 236/256, blue: 255/256,alpha: 1)),
-                                     Color(UIColor(red: 162/256, green:224/256, blue: 247/256,alpha: 1))]
-                   ),startPoint: .top, endPoint: .bottom).cornerRadius(20)
-            
-        }.padding(.trailing,0)
+		ZStack(alignment: .center){
+			LinearGradient(gradient:
+				Gradient(colors: [Color(UIColor(red: 256/256, green: 232/256, blue: 139/256,alpha: 1)),
+								  Color(UIColor(red: 256/256, green:213/256, blue: 101/256,alpha: 1))]
+			),startPoint: .top, endPoint: .bottom).cornerRadius(20).frame(width: 250 , height:468)
+			VStack{
+				HStack{
+					withAnimation{
+						Image(systemName: isSet ? "star.circle.fill" : "star.circle").resizable().frame(width: 75, height: 75).foregroundColor(.kirillGray)
+					}
+				}
+			}
+        }
         .shadow(color: Color(UIColor(red: 0, green: 0, blue: 0, alpha: 0.15)), radius: 13, x: 0, y: 4)
-        .frame(width: 200 , height:468)
+		.padding(.leading,-250)
+        
     }
 }
 
 struct favouriteCardView_Previews: PreviewProvider {
     static var previews: some View {
-        favouriteCardView()
+		favouriteCardView(isSet: .constant(true)).padding(.leading,250)
     }
 }
