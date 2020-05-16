9daf18a89ec0f23d0ed234dba637dae3bcd228e1
Кирилл <kirill-kovalev>
2020-05-16 12:46:24 +0300

    Prepared for multi-length trip search: loop for more requests with different trip length

--- flightApp/Models/APIListModel.swift
+++ flightApp/Models/APIListModel.swift
@@ -13,35 +13,42 @@ class APIListModel: TripListModel {
 	func load(){
 		print("starting load")
 		
-		guard let url = URL(string: "https://extendsclass.com/api/json-storage/bin/cdfabfc") else {
-			print("Error in generating url")
-			return;
-		}
-		URLSession.shared.dataTask(with: url, completionHandler: {
-			data , response, error in
-			if error == nil, (response as! HTTPURLResponse).statusCode == 200,data != nil {
-				UserDefaults.standard.set(data!, forKey: "cache")
-				do{
-					var decodedJSON =  try JSONDecoder().decode([TripModel].self, from: data!)
-					
-					for j in (0...decodedJSON.count-1) {
-						decodedJSON[j].localID = UUID();
-						for i in (0...decodedJSON[j].fligts.count-1){
-							decodedJSON[j].fligts[i].localID = UUID()
-						}
-					}
-					
-					self.triplist = decodedJSON
-					//print(decodedJSON)
-				}catch{
-					print(error)
-				}
-				
+		
+		self.triplist = []
+		
+		for i in (0...1) {
+			guard let url = URL(string: "https://extendsclass.com/api/json-storage/bin/cdfabfc") else {
+				print("Error in generating url")
+				return;
 			}
 			
+			URLSession.shared.dataTask(with: url, completionHandler: {
+				data , response, error in
+				if error == nil, (response as! HTTPURLResponse).statusCode == 200,data != nil {
+					UserDefaults.standard.set(data!, forKey: "cache")
+					do{
+						var decodedJSON =  try JSONDecoder().decode([TripModel].self, from: data!)
+						
+						for j in (0...decodedJSON.count-1) {
+							decodedJSON[j].localID = UUID();
+							for i in (0...decodedJSON[j].fligts.count-1){
+								decodedJSON[j].fligts[i].localID = UUID()
+							}
+						}
+						
+						self.triplist.append(contentsOf:decodedJSON)
+						//print(decodedJSON)
+					}catch{
+						print(error)
+					}
+					
+				}
 				
-			
-		}).resume();
+				
+				
+			}).resume();
+		}
+		
 		
 		return;
 	}
